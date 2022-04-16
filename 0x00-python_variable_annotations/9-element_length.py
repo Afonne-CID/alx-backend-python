db169eea091fea94f576b0aa8b7e99a4d8facf9b
Afonne-CID Paul Onyedikachi <75165644+Afonne-CID@users.noreply.github.com>
2022-04-17 00:50:39 +0100

    Create 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 9's module
+'''
+from typing import Iterable, List, Sequence, Tuple
+
+
+element_length(lst: Iterable[Sequence]) -> List[Tuple [Sequence, int]]:
+    '''Returns length of a List of sequences
+    '''
+    return [(i, len(i)) for i in lst]
