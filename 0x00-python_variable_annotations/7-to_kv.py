0844b70bfd6f12e3b444b038daa69d2a5e1e321b
Afonne-CID Paul Onyedikachi <75165644+Afonne-CID@users.noreply.github.com>
2022-04-17 00:22:10 +0100

    Fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple(str, float):
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Returns a tuple containing `k`, and `v` squared
     '''
     return (k, float(v**2))
